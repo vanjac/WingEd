2f0f9fb306f8b21d7c26d4bdeb11e1e958681bbc
j v <vantjac@gmail.com>
2023-08-21 07:03:05 -0700

    Update to OpenGL 3.2 Core profile

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -103,8 +103,8 @@ static void initGL() {
         throw winged_error(L"Failed to load OpenGL");
     if (!CHECKERR(gladLoadWGLLoader(loadGLProc, dc)))
         throw winged_error(L"Failed to load WGL extensions\n");
-    if (!GLAD_GL_VERSION_3_0)
-        throw winged_error(L"OpenGL 3.0 not available");
+    if (!GLAD_GL_VERSION_3_2)
+        throw winged_error(L"OpenGL 3.2 not available");
     CHECKERR(wglMakeCurrent(dc, NULL));
     CHECKERR(wglDeleteContext(dummyCtx));
     ReleaseDC(tempWnd, dc);
@@ -571,7 +571,8 @@ BOOL ViewportWindow::onCreate(HWND, LPCREATESTRUCT) {
     SetPixelFormat(dc, pixelFormat, &g_formatDesc);
     const int attribs[] = {
         WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
-        WGL_CONTEXT_MINOR_VERSION_ARB, 0,
+        WGL_CONTEXT_MINOR_VERSION_ARB, 2,
+        WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
 #ifdef CHROMA_DEBUG
         WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,
 #endif
