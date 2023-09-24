8b45e854519a088d93bc7cddcf7f81b9e860bc20
j v <vantjac@gmail.com>
2023-09-24 15:28:37 -0700

    Mipmaps TODO

--- src/glutil.cpp
+++ src/glutil.cpp
@@ -6,6 +6,9 @@ namespace winged {
 
 void texImageMipmaps(unsigned int target, int internalFormat, int width, int height,
         unsigned int format, unsigned int type, void *data) {
+    // TODO use either GL_GENERATE_MIPMAP or glGenerateMipmap, + confirm power of two sizing
+    // https://www.khronos.org/opengl/wiki/Common_Mistakes#gluBuild2DMipmaps
+    // https://www.gamedev.net/forums/topic/452780-gl_generate_mipmap-vs-glubuild2dmipmaps/
     gluBuild2DMipmaps(target, internalFormat, width, height, format, type, data);
 }
 
