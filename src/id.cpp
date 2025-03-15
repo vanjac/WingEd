02a70a72ab6b77d8c2f62c49478eecf370197412
j <chroma@chroma.zone>
2025-03-15 12:25:33 -0700

    Add main function for id.cpp

--- src/id.cpp
+++ src/id.cpp
@@ -9,4 +9,24 @@ id_t genId() {
     return id;
 }
 
+static void printId(id_t const& id) {
+    wprintf(L"{%08lX-%04hX-%04hX-%02hhX%02hhX-",
+        id.Data1, id.Data2, id.Data3, id.Data4[0], id.Data4[1]);
+    for (auto i = 2; i < 8; i++) {
+        wprintf(L"%02hhX", id.Data4[i]);
+    }
+    wprintf(L"}\n");
+}
+
 } // namespace
+
+#ifdef ENTRY_TEST_ID
+#include <cstdio>
+using namespace winged;
+
+int main() {
+    auto id = genId();
+    wprintf(L"Generated ID: ");
+    printId(id);
+}
+#endif // ENTRY_TEST_ID
